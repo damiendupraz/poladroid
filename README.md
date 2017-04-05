449f5865f79e2d77af3b5b753f771a51d24a388a
damien <damien@autohub.co.jp>
2017-04-05 13:52:34 +0900

    corrected the problem of orientation with iOS devices

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Poladroid is a very light and crossbrowser library to take and resize pictures on smartphones and tablets.
 It weights less than 1KB minified.
 
+Version 0.1.1
+Poladroid can now handle the orientation of pictures received from the camera of an iOS device (iPhone/iPad).
+The picture will be always shown in its intended orientation.
+The above orientation correction is made available only if the project also include the EXIF.js library to read EXIF metadata of the picture.
+
 # dependencies ?
 Poladroid is made of vanilla javascript only so it has no dependency toward Jquery or any other JS library.
 
--- sources/poladroid.js
+++ sources/poladroid.js
@@ -1,5 +1,5 @@
 /**
- *Poladroid v 0.1 
+ * Poladroid 0.1.1
  */
 (function () {
     
@@ -21,17 +21,27 @@
 		var input = null;
 		
 		//private methodes
-		var resize = function(){
-
+		var resize = function(orientation){
+			
+			//orientation can be
+			//mainly on iOS device...
+			//1 : Normal
+			//3 : bottom up
+			//6 : rotated to the left
+			//8 : rotated to the right
+			
 			var img = new Image();
 			img.onload = function () {
 			
-				
 				var ratio = img.width / img.height;
+				//in case of picture oriented to the left or right we use 1/ratio
+				if (orientation == 6 || orientation == "6" || orientation == 8 || orientation == "8")
+				ratio = 1/ratio;
 
 			    var oc = document.createElement('canvas'),
 			        octx = oc.getContext('2d');
-				
+
+					
 				if (ratio >= 1.0){
 					oc.width = width;
 					if (img.width < width)
@@ -46,8 +56,37 @@
 					oc.height = height;
 				}
 				
+				if (orientation == 6 || orientation == "6"){
+					var bak = oc.width;
+					oc.width = oc.height;
+					oc.height = bak;
+					octx.translate(oc.height * 0.5, oc.width * 0.5);
+					octx.rotate(Math.PI/2);
+					octx.translate(-oc.height * 0.5, -oc.width * 0.5);
+					octx.drawImage(img, 0, 0, oc.height, oc.width);
+				}
+
+				else if (orientation == 8 || orientation == "8"){
+					var bak = oc.width;
+					oc.width = oc.height;
+					oc.height = bak;
+					octx.translate(oc.height * 0.5, oc.width * 0.5);
+					octx.rotate(-Math.PI/2);
+					octx.translate(-oc.height * 0.5, -oc.width * 0.5);
+					octx.drawImage(img, 0, 0, oc.height, oc.width);
+				}			
+				
+				else if (orientation == 3 || orientation == "3"){
+					octx.translate(oc.width * 0.5, oc.height * 0.5);
+					octx.rotate(Math.PI);
+					octx.translate(-oc.width * 0.5, -oc.height * 0.5);
+					octx.drawImage(img, 0, 0, oc.width, oc.height);
+				}
+				
+				else
 			    octx.drawImage(img, 0, 0, oc.width, oc.height);
-			
+				
+				
 				base64data = oc.toDataURL("image/jpeg", Math.round(quality * 100) / 10000);
 						   
                 if (callback != null)
@@ -78,7 +117,22 @@
 					var fileReader = new FileReader();
 					fileReader.onload = function (event) {
                             base64data = event.target.result;
-                            resize();
+                            
+                            //check if the EXIF reader exists
+                            if (!window.EXIF)
+                            resize(1); 
+                            else
+                            EXIF.getData(fileObject, function () {
+                            	var orientation = 1;
+                            	if (this.hasOwnProperty("exifdata"))
+                            	if (this["exifdata"].hasOwnProperty("Orientation"))
+                            	orientation = this["exifdata"]["Orientation"];
+                            	
+	    						
+	    						resize(orientation);                        
+							});
+                            
+                            
 
                     };
                     fileReader.readAsDataURL(fileObject);
